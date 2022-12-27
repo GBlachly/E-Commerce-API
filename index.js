aba630f11084d32f3b6230ab3d01be941f8564aa
GBlachly <gblachly4@gmail.com>
2022-12-27 18:35:33 -0500

    Initialized carts Router

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ const PORT = process.env.port || 4001;
 const usersRouter = require('./routers/usersRouter.js');
 const productsRouter = require('./routers/productsRouter.js');
 const ordersRouter = require('./routers/ordersRouter.js');
-
+const cartsRouter = require('./routers/cartsRouter.js');
 
 
 
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
 app.use('/orders', ordersRouter);
+app.use('/carts', cartsRouter);
 
 
 app.listen(PORT, () => {
