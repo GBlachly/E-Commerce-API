06e82a6b1463c9d6d41a19d1c34e9ed4ba403644
GBlachly <gblachly4@gmail.com>
2022-12-27 18:26:02 -0500

    Initialized orders, products, and users Routers

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const PORT = process.env.port || 4001;
 
+const usersRouter = require('./routers/usersRouter.js');
+const productsRouter = require('./routers/productsRouter.js');
+const ordersRouter = require('./routers/ordersRouter.js');
 
 
 
+
+app.use('/users', usersRouter);
+app.use('/products', productsRouter);
+app.use('/orders', ordersRouter);
 
 
 app.listen(PORT, () => {
