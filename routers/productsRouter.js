edbfd860a5c956fe80c4ac41bfeb10c5d35c6493
GBlachly <gblachly4@gmail.com>
2023-01-02 18:29:59 -0500

    Added and tested GET product by Id route

--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -5,8 +5,17 @@ const db = require('../db/db');
 
 //ROUTES
 productsRouter.get('/:productId', (req, res, next) => {
+    let productId = Number(req.params.productId);
 
+    db.query('SELECT * FROM products WHERE id = $1', [productId], (err, result) => {
+        if (err) {
+            return next(err)
+        }
+        res.status(200).json( result.rows[0] );
+    });
 });
+
+
 
 
 module.exports = productsRouter;
