6a83cb1bbc3f716b9e72bf585e083978f5f238d7
GBlachly <gblachly4@gmail.com>
2023-01-03 16:57:47 -0500

    Created a productsModel to host query logic

---
+++ models/productsModel.js
@@ -0,0 +1,73 @@
+const db = require('../db/db');
+
+
+const getAllProducts = (req, res, next) => {
+    db.query('SELECT * FROM products;', [], (err, result) => {
+        if (err) {
+            return next(err)
+        }
+        res.status(200).json( result.rows );
+    });
+
+};
+
+
+const getProductByID = (req, res, next) => {
+    const productId = Number(req.params.productId);
+    
+    db.query('SELECT * FROM products WHERE id = $1;', [productId], (err, result) => {
+        if (err) {
+            return next(err)
+        }
+        res.status(200).json( result.rows[0] );
+    });
+};
+
+
+const createProduct = (req, res, next) => {
+    const { name, price, stock } = req.body;
+
+    db.query('INSERT INTO products (name, price, stock) VALUES ($1, $2, $3) RETURNING *;', [name, price, stock], (err, result) => {
+        if (err) {
+            return next(err)
+        }
+        res.status(201).send(`Product created with ID: ${result.rows[0].id}`)
+    });
+    
+};
+
+
+const updateProduct = (req, res, next) => {
+    const productId = Number(req.params.productId);
+    const { name, price, stock } = req.body;
+
+    db.query('ALTER TABLE products SET name = $2, price = $3, stock = $4 WHERE id = $1;', [productId, name, price, stock], (err, result) => {
+        if (err) {
+            return next(err)
+        }
+        res.status(200).send(`Product with ID: ${productId} was updated`)
+    });
+    
+};
+
+
+const deleteProduct = (req, res, next) => {
+    const productId = Number(req.params.productId);
+
+    db.query('DELETE FROM products WHERE id = $1;', [productId], (err, result) => {
+        if (err) {
+            return next(err)
+        }
+        res.status(200).send(`Product with ID: ${productId} was deleted`)
+    });
+    
+};
+
+
+module.exports = {
+    getAllProducts,
+    getProductByID,
+    createProduct, 
+    updateProduct,
+    deleteProduct
+};
--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -1,73 +1,30 @@
 const express = require('express');
 const productsRouter = express.Router();
 
-const db = require('../db/db');
+const {
+    getAllProducts,
+    getProductByID,
+    createProduct, 
+    updateProduct,
+    deleteProduct
+} = require('../models/productsModel');
 
 // ROUTES
 
 /* get all products */
-productsRouter.get('/', (req, res, next) => {
-    db.query('SELECT * FROM products;', [], (err, result) => {
-        if (err) {
-            return next(err)
-        }
-        res.status(200).json( result.rows );
-    });
-});
-
+productsRouter.get('/', getAllProducts);
 
 /* get product by id */
-productsRouter.get('/:productId', (req, res, next) => {
-    let productId = Number(req.params.productId);
-
-    db.query('SELECT * FROM products WHERE id = $1;', [productId], (err, result) => {
-        if (err) {
-            return next(err)
-        }
-        res.status(200).json( result.rows[0] );
-    });
-});
-
+productsRouter.get('/:productId', getProductByID);
 
 /* create new product (admin only) */
-productsRouter.post('/', (req, res, next) => {
-    const { name, price, stock } = req.body;
-
-    db.query('INSERT INTO products (name, price, stock) VALUES ($1, $2, $3) RETURNING *;', [name, price, stock], (err, result) => {
-        if (err) {
-            return next(err)
-        }
-        res.status(201).send(`Product created with ID: ${result.rows[0].id}`)
-    });
-});
-
+productsRouter.post('/', createProduct);
 
 /* update product (admin only/ except for stock which should update when product is added to an order) */
-productsRouter.put('/:productId', (req, res, next) => {
-    const productId = Number(req.params.productId);
-    const { name, price, stock } = req.body;
-
-    db.query('ALTER TABLE products SET name = $2, price = $3, stock = $4 WHERE id = $1;', [productId, name, price, stock], (err, result) => {
-        if (err) {
-            return next(err)
-        }
-        res.status(200).send(`Product with ID: ${productId} was updated`)
-    });
-});
-
+productsRouter.put('/:productId', updateProduct);
 
 /* delete product by Id (admin only) */
-productsRouter.delete('/:productId', (req, res, next) => {
-    let productId = Number(req.params.productId);
-
-    db.query('DELETE FROM products WHERE id = $1;', [productId], (err, result) => {
-        if (err) {
-            return next(err)
-        }
-        res.status(200).send(`Product with ID: ${productId} was deleted`)
-    });
-});
-
+productsRouter.delete('/:productId', deleteProduct);
 
 
 module.exports = productsRouter;
