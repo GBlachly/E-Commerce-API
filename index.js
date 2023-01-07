47a121a5e7ef97efbb233392085cf69def17698a
GBlachly <gblachly4@gmail.com>
2023-01-06 20:55:32 -0500

    Installed and added passport

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const session = require('express-session');
 const store = new session.MemoryStore();
 /* Note: Storing in-memory sessions is something that should be done only 
 during development, NOT during production due to security risks. */
+
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan'); 
@@ -24,6 +28,9 @@ app.use(
     store
   })
 );
+
+app.use(passport.initialize()); // notes 4.7 pg. 78
+app.use(passport.session());
 
 app.use(cors()); //Not sure where this goes exactly or if i actually need it 
 app.use(morgan('dev'));
