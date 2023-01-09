4b5927073fa978b7b2f410980a34846011e69acf
GBlachly <gblachly4@gmail.com>
2023-01-09 17:54:33 -0500

    Added missing callback to handle err in req.logout

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -8,19 +8,23 @@ const { registerUser } = require('../models/usersModel');
 
 /* logout user */
 usersRouter.get("/logout", (req, res) => {
-    req.logout();
+    req.logout((err) => {
+        if (err) return next(err);
+    });
     res.status(200).send('User logged out');
     //res.redirect("/");
-  });
+  }
+);
   
 
 /* load login page  */
 usersRouter.get('/login', (req, res, next) => {
     res.status(200).send('Welcome to the Login Page');
+    //res.render();
 });
 
 
-/* login user  */
+/* login user with local strategy */
 usersRouter.post(
     '/login', 
     passport.authenticate("local"/*, { failureRedirect: '/login', failureMessage: true} */), 
@@ -29,6 +33,9 @@ usersRouter.post(
     }
 );
 
+/* login user with google strategy */
+
+/* login user with facebook strategy */
 
 /* create new user (authentication of user not needed) */
 usersRouter.post('/register', registerUser);
