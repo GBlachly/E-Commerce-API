45f3ae6ec797ac82e70c98c1c15a92b310649386
GBlachly <gblachly4@gmail.com>
2023-01-10 18:10:32 -0500

    Small spacing edit in db.js

--- db/db.js
+++ db/db.js
@@ -12,7 +12,8 @@ module.exports = {
       return pool.query(text, params, callback)
     },
 
-    //I dont think this is necessary. Pretty sure you can use the first pool.query function and just not included the callback as the third parameter
+    /*I dont think this is necessary. Pretty sure you can use the first pool.query function 
+    and just not included the callback as the third parameter */
     queryNoCB: (text, params) => {
       return pool.query(text, params)
     },
